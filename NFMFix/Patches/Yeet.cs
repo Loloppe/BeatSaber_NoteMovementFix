2bfa77f91e47d5b4be8c44685bf610386857fdd6
Loloppe <LaurentBlaisCote@outlook.com>
2023-03-16 20:27:49 -0400

    funny

--- NFMFix/Patches/Yeet.cs
+++ NFMFix/Patches/Yeet.cs
@@ -10,7 +10,7 @@ namespace NoteMovementFix.Patches
     {
         static bool Prefix(ref float start, ref float end, ref float headOffsetZ, ref float t, ref float __result)
         {
-            if(Config.Instance.Enabled)
+            if(Config.Instance.Enabled && !Plugin.InReplay)
             {
                 __result = Mathf.LerpUnclamped(start + headOffsetZ, end + headOffsetZ, t);
                 return false;
@@ -110,6 +110,111 @@ namespace NoteMovementFix.Patches
                 }
                 Vector3 vector2 = noteCutInfo.worldRotation * (vector + new Vector3(0f, num, 7.55f));
                 flyingScoreEffect.InitAndPresent(cutScoreBuffer, 0.7f, vector2, color);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    // Fix NoteJump to match player position
+    [HarmonyPatch(typeof(NoteJump), nameof(NoteJump.ManualUpdate))]
+    internal class Yeet5
+    {
+        static bool Prefix(ref IAudioTimeSource ____audioTimeSyncController, ref float ____jumpDuration, ref float ____beatTime, ref Vector3 ____startPos, ref Vector3 ____endPos, ref Vector3 __result,
+            ref Vector3 ____localPosition, ref NoteJump __instance, ref Quaternion ____inverseWorldRotation, ref Action ___noteJumpDidPassHalfEvent, ref PlayerTransforms ____playerTransforms,
+            ref float ____startVerticalVelocity, ref float ____yAvoidance, ref float ____gravity, ref Quaternion ____startRotation, ref Quaternion ____middleRotation, ref Quaternion ____endRotation,
+            ref bool ____rotateTowardsPlayer, ref PlayerSpaceConvertor ____playerSpaceConvertor, ref Transform ____rotatedObject, ref bool ____halfJumpMarkReported, ref bool ____threeQuartersMarkReported,
+            ref bool ____missedMarkReported, ref float ____missedTime, ref Action<NoteJump> ___noteJumpDidPassThreeQuartersEvent, ref Action ___noteJumpDidPassMissedMarkerEvent, ref Action ___noteJumpDidFinishEvent,
+            ref Quaternion ____worldRotation, ref Action<float> ___noteJumpDidUpdateProgressEvent, ref float ____endDistanceOffset)
+        {
+            if (Config.Instance.Enabled && !Plugin.InReplay)
+            {
+                float songTime = ____audioTimeSyncController.songTime;
+                float num = songTime - (____beatTime - ____jumpDuration * 0.5f);
+                float num2 = num / ____jumpDuration;
+                if (____startPos.x == ____endPos.x)
+                {
+                    ____localPosition.x = ____startPos.x;
+                }
+                else if (num2 < 0.25f)
+                {
+                    ____localPosition.x = ____startPos.x + (____endPos.x - ____startPos.x) * Easing.InOutQuad(num2 * 4f);
+                }
+                else
+                {
+                    ____localPosition.x = ____endPos.x;
+                }
+                ____localPosition.z = ____playerTransforms.MoveTowardsHead(____startPos.z, ____endPos.z, ____inverseWorldRotation, num2);
+                ____localPosition.y = ____startPos.y + ____startVerticalVelocity * num - ____gravity * num * num * 0.5f;
+                if (____yAvoidance != 0f && num2 < 0.25f)
+                {
+                    float num3 = 0.5f - Mathf.Cos(num2 * 8f * Mathf.PI) * 0.5f;
+                    ____localPosition.y += num3 * ____yAvoidance;
+                }
+                if (num2 < 0.5f)
+                {
+                    Quaternion quaternion;
+                    if (num2 < 0.125f)
+                    {
+                        quaternion = Quaternion.Slerp(____startRotation, ____middleRotation, Mathf.Sin(num2 * 3.1415927f * 4f));
+                    }
+                    else
+                    {
+                        quaternion = Quaternion.Slerp(____middleRotation, ____endRotation, Mathf.Sin((num2 - 0.125f) * 3.1415927f * 2f));
+                    }
+                    if (____rotateTowardsPlayer)
+                    {
+                        Vector3 vector = ____playerTransforms.headPseudoLocalPos;
+                        vector.y = Mathf.Lerp(vector.y, ____localPosition.y, 0.8f);
+                        vector = ____inverseWorldRotation * vector;
+                        Vector3 normalized = (____localPosition - vector).normalized;
+                        Quaternion quaternion2 = default;
+                        Vector3 vector2 = ____playerSpaceConvertor.worldToPlayerSpaceRotation * ____rotatedObject.up;
+                        quaternion2.SetLookRotation(normalized, ____inverseWorldRotation * vector2);
+                        ____rotatedObject.localRotation = Quaternion.Lerp(quaternion, quaternion2, num2 * 2f);
+                    }
+                    else
+                    {
+                        ____rotatedObject.localRotation = quaternion;
+                    }
+                }
+                if (num2 >= 0.5f && !____halfJumpMarkReported)
+                {
+                    ____halfJumpMarkReported = true;
+                    ___noteJumpDidPassHalfEvent?.Invoke();
+                }
+                if (num2 >= 0.75f && !____threeQuartersMarkReported)
+                {
+                    ____threeQuartersMarkReported = true;
+                    ___noteJumpDidPassThreeQuartersEvent?.Invoke(__instance);
+                }
+                if (songTime >= ____missedTime && !____missedMarkReported)
+                {
+                    ____missedMarkReported = true;
+                    ___noteJumpDidPassMissedMarkerEvent?.Invoke();
+                }
+                if (____threeQuartersMarkReported)
+                {
+                    float num4 = (num2 - 0.75f) / 0.25f;
+                    num4 = num4 * num4 * num4;
+                    ____localPosition.z -= Mathf.LerpUnclamped(0f, ____endDistanceOffset, num4);
+                }
+                if (num2 >= 1f)
+                {
+                    if (!____missedMarkReported)
+                    {
+                        ____missedMarkReported = true;
+                        ___noteJumpDidPassMissedMarkerEvent?.Invoke();
+                    }
+                    ___noteJumpDidFinishEvent?.Invoke();
+                }
+                Vector3 vector3 = ____worldRotation * ____localPosition;
+                __instance.transform.localPosition = vector3;
+                ___noteJumpDidUpdateProgressEvent?.Invoke(num2);
+
+                __result = vector3;
 
                 return false;
             }
